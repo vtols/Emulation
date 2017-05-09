cc5abd3b38131ddfca4532edf7210b2f31eec253
Valery Tolstov <me@vtolstov.org>
2017-05-09 23:30:10 +0300

    Combine hardware

--- src/emulation/i8080/Main.java
+++ src/emulation/i8080/Main.java
@@ -1,6 +1,5 @@
 package emulation.i8080;
 
-import emulation.i8080.cpu.Cpu8080;
 import emulation.i8080.hardware.vesta.*;
 import emulation.util.FileLoader;
 
@@ -8,23 +7,11 @@ public class Main {
     public static void main(String[] args) {
         String romPath = args[0];
         String casPath = args[1];
-        int startPc = Integer.parseInt(args[2]);
         byte[] rom = FileLoader.load(romPath);
         byte[] cas = FileLoader.load(casPath);
 
-        VestaMemory vsmem = new VestaMemory(rom);
-        VestaKeyboard kb = new VestaKeyboard();
-        VestaVideo video = new VestaVideo(vsmem.getRam(), kb);
-        VestaTape tape = new VestaTape(cas);
-        Cpu8080 c = new Cpu8080(vsmem);
-        VestaTimer tm = new VestaTimer(c);
-        c.setPort(0x80, vsmem.getModePort());
-        c.setPort(0x81, kb.getKeyboardModePort());
-        c.setPort(0x82, kb.getKeyboardPort());
-        c.setPort(0x84, video.getModePort());
-        c.setPort(0x8d, tape.getTapePort());
-
-        tm.enable();
-        c.run((short) startPc);
+        VestaHardware hardware = new VestaHardware(rom);
+        hardware.loadCas(cas);
+        hardware.run();
     }
 }
---
+++ src/emulation/i8080/hardware/vesta/VestaHardware.java
@@ -0,0 +1,36 @@
+package emulation.i8080.hardware.vesta;
+
+import emulation.i8080.cpu.Cpu8080;
+
+public class VestaHardware {
+    VestaMemory vsmem;
+    VestaKeyboard keyboard;
+    VestaVideo video;
+    VestaTape tape;
+    Cpu8080 cpu;
+    VestaTimer timer;
+
+    public VestaHardware(byte[] rom) {
+        vsmem = new VestaMemory(rom);
+        cpu = new Cpu8080(vsmem);
+        keyboard = new VestaKeyboard();
+        video = new VestaVideo(vsmem.getRam(), keyboard);
+        timer = new VestaTimer(cpu);
+
+
+        cpu.setPort(0x80, vsmem.getModePort());
+        cpu.setPort(0x81, keyboard.getKeyboardModePort());
+        cpu.setPort(0x82, keyboard.getKeyboardPort());
+        cpu.setPort(0x84, video.getModePort());
+    }
+
+    public void loadCas(byte[] cas) {
+        tape = new VestaTape(cas);
+        cpu.setPort(0x8d, tape.getTapePort());
+    }
+
+    public void run() {
+        timer.enable();
+        cpu.run((short) 0);
+    }
+}
